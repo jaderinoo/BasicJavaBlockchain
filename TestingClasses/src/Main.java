6d6a5e0b4bc4a2c8a229d54aaff43e486377019c
Jad El-Khatib <jaderinoo@gmail.com>
2019-08-15 05:09:07 -0400

    Cleanup

--- TestingClasses/src/Main.java
+++ TestingClasses/src/Main.java
@@ -2,7 +2,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
